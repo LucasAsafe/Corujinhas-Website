2424a421b1f5420f59ad096c1fd7a89d6a0406ad
Lucas Asafe <lucas1d2013@gmail.com>
2020-01-14 17:34:00 -0300

    scroll suave back to top section de contato header

--- js/main.js
+++ js/main.js
@@ -44,7 +44,21 @@ $(window).on('load', function() {
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
 
-	
+
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		  if (target.length) {
+			$('html, body').animate({
+			  scrollTop: (target.offset().top)
+			}, 1000, "easeInOutExpo");
+			return false;
+		  }
+		}
+	  });
+
+
 	$('.hero-slider').owlCarousel({
 		loop: true,
 		nav: false,
@@ -58,3 +72,21 @@ $(window).on('load', function() {
 
 })(jQuery);
 
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
